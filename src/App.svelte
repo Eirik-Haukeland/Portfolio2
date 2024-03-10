070fb0cbff1d2ed70beca29d13e4515df624032d
Eirik Berget Haukeland <eirik@haukeland.name>
2024-03-10 21:47:56 +0100

    added breakpoints

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,47 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import holidazeImg from './assets/holidaze-09-03-24.png'
+  import ShopATonImg from './assets/shop-a-ton-2024-03-09.png'
+  import gotStoreImg from './assets/Got-store-2024-03-09.png'
+  import Card from './components/Card.svelte'
+
+  const pagesInfo = [
+    {
+      imgSrc: holidazeImg,
+      imgAlt: 'image of home page for "holidaze"',
+      projectTitle: 'holidaze',
+      repoUrl: 'https://github.com/Eirik-Haukeland/ProjectExam2',
+      liveUrl: 'https://main--ebhholidaze.netlify.app/',
+    }, 
+    {
+      imgSrc: gotStoreImg,
+      imgAlt: 'image of home page to "Got Store"',
+      projectTitle: 'Got Store',
+      repoUrl: 'https://github.com/Eirik-Haukeland/front-end-frameworks-ca',
+      liveUrl: 'https://main--astounding-kleicha-407922.netlify.app/',
+    }, 
+    {
+      imgSrc: ShopATonImg,
+      imgAlt: 'this is a image of the home page for "Shop a Ton"',
+      projectTitle: 'Shop a Ton',
+      repoUrl: 'https://github.com/Eirik-Haukeland/semester-project-2',
+      liveUrl: 'https://eirik-haukeland.github.io/semester-project-2/',
+    }, 
+  ]
 </script>
 
-<main class="flex justify-center">
-  <h1 class="text-5xl font-bold underline">Hello World!</h1>
-  <section class="flex flex-wrap">
+<main class="flex flex-col gap-7 w-fit mt-7 mx-auto">
+  <section class="flex flex-col gap-3">
+    <h1 class="text-4xl sm:text-5xl 2xl:text-6xl font-bold">Portfolio Page</h1>
+    <a href="https://github.com/Eirik-Haukeland" class="text-blue-500 hover:text-blue-800 focus:text-blue-800 2xl:text-xl">for: Eirik Berget Haukeland</a>
+  </section>
+  <section class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    {#each pagesInfo as page (page.projectTitle)}
+      <Card
+        imgSrc={page.imgSrc}
+        imgAlt={page.imgAlt}
+        projectTitle={page.projectTitle}
+        repoUrl={page.repoUrl}
+        liveUrl={page.liveUrl} />
+    {/each}
   </section>
 </main>
-
-<style>
-</style>
--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,11 +6,11 @@
   export let liveUrl = ''
 </script>
 
-<div class="w-64 h-min p-2 bg-zinc-50 flex flex-col rounded">
+<div class="w-72 xl:w-96 h-min p-2 bg-zinc-100 flex flex-col rounded">
   <img src={imgSrc} alt={imgAlt}>
-  <h3>{projectTitle}</h3>
-  <a href={repoUrl} class="text-blue-500">link to repo for {projectTitle}</a>
+  <h3 class="capitalize text-xl font-bold">{projectTitle}</h3>
+  <a href={repoUrl} class="text-blue-500 hover:text-blue-800 focus:text-blue-800">repo for {projectTitle}</a>
   {#if liveUrl !== ''}
-    <a href={liveUrl} class="text-blue-500">link to livePage for {projectTitle}</a>
+    <a href={liveUrl} class="text-blue-500 hover:text-blue-800 focus:text-blue-800">livePage for {projectTitle}</a>
   {/if}
 </div>
