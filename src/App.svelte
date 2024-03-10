7f4fcee05d82900a1c7f2cc2e64a18307e09c244
Eirik Berget Haukeland <eirik@haukeland.name>
2024-03-10 16:43:32 +0100

    inicilsed with vite and tailwind

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="flex justify-center">
+  <h1 class="text-5xl font-bold underline">Hello World!</h1>
+  <section class="flex flex-wrap">
+  </section>
+</main>
+
+<style>
+</style>
