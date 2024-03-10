4fff595edeb90cf77af68f2fe7b570bec08d0891
Eirik Berget Haukeland <eirik@haukeland.name>
2024-03-10 18:51:24 +0100

    added images of the sites running and a card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let imgSrc
+  export let imgAlt
+  export let projectTitle
+  export let repoUrl
+  export let liveUrl = ''
+</script>
+
+<div class="w-64 h-min p-2 bg-zinc-50 flex flex-col rounded">
+  <img src={imgSrc} alt={imgAlt}>
+  <h3>{projectTitle}</h3>
+  <a href={repoUrl} class="text-blue-500">link to repo for {projectTitle}</a>
+  {#if liveUrl !== ''}
+    <a href={liveUrl} class="text-blue-500">link to livePage for {projectTitle}</a>
+  {/if}
+</div>
